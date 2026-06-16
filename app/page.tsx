e5b783a6068554e2da00ecdb5856bd5b31cc290d
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-06-16 13:25:05 -0700

    beginning part 6

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,8 @@ export default function Home() {
   return (
   <>
   <div className="min-h-screen text-white">
-    <div className="">
+    <div className="container mx-auto px-4 py-6 max-w-7xl">
+
       <Header />
       
     </div>
