23e18b717e91988ff5a0709fc8794b6b97791589
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-06-17 16:18:58 -0700

    beginning part 11

--- app/page.tsx
+++ app/page.tsx
@@ -64,15 +64,65 @@ export default function Home() {
     <div className="container mx-auto px-4 py-6 max-w-7xl">
 
       <Header units={units} onToggleUnits={toggleUnits} />
+      <div className='text-center mb-8'>
+        <h1 className='text-3xl md:text-5xl font-bold mb-16  '>
+          How's the sky looking today?
+        </h1>
+        <Search />
+        {/* <Search
+            onSearch={handleSearch}
+            loading={loading}
+            searchResults={searchResults}
+            showSearchResults={showSearchResults}
+            onLocationSelect={handleLocationSelect}
+            onCloseResults={() => setShowSearchResults(false)}
+          /> */}
+      </div>
+
+{/* { weatherData ? */}
+
+       {/* <>
+       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className='lg:col-span-2 space-y-6 ' >
+          <CurrentWeather />
+          <Metrics /> 
+          <DailyForecast />
+
+        </div>
+       <div className='lg:col-span-1'>
+        <HourlyForecast />
+       </div>
+       </div>
+       </>  */}
+
+       {/* : */}
+
+       <>
+       <div className="flex flex-col items-center text-center max-w-lg mx-auto space-y-4 py-12">
+            <Image src={errorIcon} alt="Error" className="block mx-auto size-10" />
+            <h2 className="text-4xl font-bold">Something went wrong</h2>
+            <p className="text-gray-400">
+              We couldn't connect to the server (API error). Please try again in a few moments.
+            </p>
+            <button
+              onClick={loadWeatherData}
+              className="flex items-center justify-center space-x-2 glass-card px-5 py-2.5 rounded-lg hover:bg-white/20 transition-all duration-200 font-medium"
+            >
+              <Image src={retryIcon} alt="" className="mr-2 size-4" />
+              <span>Retry Connection</span>
+            </button>
+        </div>
+
+       </>
+
+{/* } */}
+
+
       
     </div>
     
 
-    <Search />
-    <CurrentWeather />
-    <Metrics />
-    <DailyForecast />
-    <HourlyForecast />
+   
 
 
 
